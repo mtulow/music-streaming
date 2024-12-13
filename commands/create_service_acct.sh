b0fb8223045f20884da40096b023f6bcec3e97d6
Mut Tulow <mtulow@icloud.com>
2024-12-13 07:15:23 -0700

    Created infrastructure objects.

--- commands/create_service_acct.sh
+++ commands/create_service_acct.sh
@@ -1,15 +1,20 @@
 
+# Create the infrastructure service account
+gcloud iam service-accounts create infra-service-account \
+    --description="Service account to use for the terraform client." \
+    --display-name="infra-service-account"
+
 # Create the pipeline service account
-gcloud iam service-accounts create <prefect-service-account> \
+gcloud iam service-accounts create prefect-service-account \
     --description="Service account to use for the prefect worker." \
     --display-name="prefect-service-account"
 
 #  service account has the proper permissions to execute flow runs as Cloud Run jobs
-gcloud projects add-iam-policy-binding <PROJECT-ID> \
-    --member="serviceAccount:<SERVICE-ACCOUNT-NAME>@<PROJECT-ID>.iam.gserviceaccount.com" \
+gcloud projects add-iam-policy-binding music-streaming-project-444609 \
+    --member="serviceAccount:prefect-service-account@music-streaming-project-444609.iam.gserviceaccount.com" \
     --role="roles/iam.serviceAccountUser"
 
-gcloud projects add-iam-policy-binding <PROJECT-ID> \
-    --member="serviceAccount:<SERVICE-ACCOUNT-NAME>@<PROJECT-ID>.iam.gserviceaccount.com" \
+gcloud projects add-iam-policy-binding music-streaming-project-444609 \
+    --member="serviceAccount:prefect-service-account@music-streaming-project-444609.iam.gserviceaccount.com" \
     --role="roles/run.admin"
     
